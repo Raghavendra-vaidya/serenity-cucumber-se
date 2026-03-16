0ba2797d4174e4c791da8c679298f5d8692f518a
Raghavendra Vaidya <rkvaidya58@gmail.com>
2026-03-16 13:25:10 +0530

    fix reports :zap:

--- src/test/java/org/rv/runner/TestRunnerSuite.java
+++ src/test/java/org/rv/runner/TestRunnerSuite.java
@@ -1,17 +1,14 @@
 package org.rv.runner;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.*;
 
 import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("/features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber.html, json:target/cucumber.json, io.cucumber.core.plugin.SerenityReporterParallel")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.rv.steps")
 public class TestRunnerSuite {
 }
